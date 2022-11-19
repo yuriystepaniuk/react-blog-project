96c092e861ecbef8ce589aee6767313ccca321ff
yura <yur.stepanyuk@gmail.com>
2022-11-20 01:15:50 +0200

    fix slider again

--- src/components/Slider/SwiperSlider.tsx
+++ src/components/Slider/SwiperSlider.tsx
@@ -2,17 +2,23 @@ import { Swiper, SwiperSlide } from "swiper/react"
 
 import "swiper/css"
 
-export default () => {
+type Props = {}
+
+const SwiperSlider = (props: Props) => {
     return (
         <Swiper
-        
             spaceBetween={10}
             onSlideChange={() => console.log("slide change")}
             onSwiper={(swiper) => console.log(swiper)}
         >
-            <SwiperSlide><img src="images/autor.png" alt="" /></SwiperSlide>
-            <SwiperSlide><img src="images/autor2.png" alt="" /></SwiperSlide>
+            <SwiperSlide>
+                <img src="images/autor.png" alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src="images/autor2.png" alt="" />
+            </SwiperSlide>
         </Swiper>
     )
 }
 
+export default SwiperSlider
