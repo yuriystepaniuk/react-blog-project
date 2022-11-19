d4af595419473c41d9a88999dd4932172b17dea2
yura <yur.stepanyuk@gmail.com>
2022-11-20 01:21:01 +0200

    add pagination,naviagation to slider

--- src/components/Slider/SwiperSlider.tsx
+++ src/components/Slider/SwiperSlider.tsx
@@ -1,15 +1,23 @@
 import { Swiper, SwiperSlide } from "swiper/react"
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 
 import "swiper/css"
+import "swiper/css/navigation"
+import "swiper/css/pagination"
+import "swiper/css/scrollbar"
 
 type Props = {}
 
 const SwiperSlider = (props: Props) => {
     return (
         <Swiper
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
             spaceBetween={10}
-            onSlideChange={() => console.log("slide change")}
+            navigation
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
             onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log("slide change")}
         >
             <SwiperSlide>
                 <img src="images/autor.png" alt="" />
