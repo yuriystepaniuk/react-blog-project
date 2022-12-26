7f09d07a54d8615807da0901204ce1bc2250f1af
yura <yur.stepanyuk@gmail.com>
2022-12-26 11:42:05 +0200

    delete excess block from Reviews component

--- src/components/Reviews/Reviews.tsx
+++ src/components/Reviews/Reviews.tsx
@@ -11,25 +11,6 @@ import React, { useState } from "react"
 import "./Reviews.scss"
 import { styled } from "@mui/material/styles"
 
-const CssTextField = styled(TextField)({
-    "& label.Mui-focused": {
-      color: "white"
-    },
-    "& .MuiInput-underline:after": {
-      borderBottomColor: "white"
-    },
-    "& .MuiOutlinedInput-root": {
-      "& fieldset": {
-        borderColor: "white"
-      },
-      "&:hover fieldset": {
-        borderColor: "white"
-      },
-      "&.Mui-focused fieldset": {
-        borderColor: "white"
-      }
-    }
-  });
 
 type Props = {}
 
